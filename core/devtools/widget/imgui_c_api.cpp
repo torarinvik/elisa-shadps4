79e0fdcb831a3261b3fdfa882fd559f6eaf6eda4
torarinvik <torarinvik@gmail.com>
2026-05-18 13:30:06 +0200

    Add ImGui C ABI for devtools widgets

---
+++ core/devtools/widget/imgui_c_api.cpp
@@ -0,0 +1,220 @@
+// SPDX-FileCopyrightText: Copyright 2026 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "imgui_c_api.h"
+
+#include <imgui.h>
+
+extern "C" {
+
+void elisa_imgui_set_next_window_size(float x, float y, int cond) {
+    ImGui::SetNextWindowSize(ImVec2{x, y}, static_cast<ImGuiCond>(cond));
+}
+
+bool elisa_imgui_begin(const char* name, bool* open, int flags) {
+    return ImGui::Begin(name, open, static_cast<ImGuiWindowFlags>(flags));
+}
+
+void elisa_imgui_end() {
+    ImGui::End();
+}
+
+bool elisa_imgui_begin_child(const char* id, float x, float y, bool border, int flags) {
+    return ImGui::BeginChild(id, ImVec2{x, y}, border, static_cast<ImGuiWindowFlags>(flags));
+}
+
+void elisa_imgui_end_child() {
+    ImGui::EndChild();
+}
+
+bool elisa_imgui_begin_table(const char* id, int columns, int flags) {
+    return ImGui::BeginTable(id, columns, static_cast<ImGuiTableFlags>(flags));
+}
+
+void elisa_imgui_end_table() {
+    ImGui::EndTable();
+}
+
+void elisa_imgui_table_setup_column(const char* label, int flags, float init_width_or_weight) {
+    ImGui::TableSetupColumn(label, static_cast<ImGuiTableColumnFlags>(flags),
+                            init_width_or_weight);
+}
+
+void elisa_imgui_table_headers_row() {
+    ImGui::TableHeadersRow();
+}
+
+void elisa_imgui_table_next_row() {
+    ImGui::TableNextRow();
+}
+
+void elisa_imgui_table_next_column() {
+    ImGui::TableNextColumn();
+}
+
+void elisa_imgui_table_set_column_index(int column) {
+    ImGui::TableSetColumnIndex(column);
+}
+
+void elisa_imgui_text_unformatted(const char* text) {
+    ImGui::TextUnformatted(text);
+}
+
+void elisa_imgui_text(const char* fmt) {
+    ImGui::TextUnformatted(fmt);
+}
+
+void elisa_imgui_text_colored(float r, float g, float b, float a, const char* text) {
+    ImGui::TextColored(ImVec4{r, g, b, a}, "%s", text);
+}
+
+bool elisa_imgui_button(const char* label) {
+    return ImGui::Button(label);
+}
+
+bool elisa_imgui_checkbox(const char* label, bool* value) {
+    return ImGui::Checkbox(label, value);
+}
+
+bool elisa_imgui_input_text(const char* label, char* buffer, uintptr_t buffer_size, int flags) {
+    return ImGui::InputText(label, buffer, static_cast<size_t>(buffer_size),
+                            static_cast<ImGuiInputTextFlags>(flags));
+}
+
+bool elisa_imgui_tree_node(const void* id, const char* label) {
+    return ImGui::TreeNode(id, "%s", label);
+}
+
+void elisa_imgui_tree_pop() {
+    ImGui::TreePop();
+}
+
+bool elisa_imgui_selectable(const char* label, bool selected, int flags, float x, float y) {
+    return ImGui::Selectable(label, selected, static_cast<ImGuiSelectableFlags>(flags),
+                             ImVec2{x, y});
+}
+
+bool elisa_imgui_collapsing_header(const char* label, int flags) {
+    return ImGui::CollapsingHeader(label, static_cast<ImGuiTreeNodeFlags>(flags));
+}
+
+void elisa_imgui_same_line(float offset_from_start_x, float spacing) {
+    ImGui::SameLine(offset_from_start_x, spacing);
+}
+
+void elisa_imgui_separator() {
+    ImGui::Separator();
+}
+
+void elisa_imgui_separator_text(const char* label) {
+    ImGui::SeparatorText(label);
+}
+
+void elisa_imgui_push_id_int(int id) {
+    ImGui::PushID(id);
+}
+
+void elisa_imgui_push_id_ptr(const void* id) {
+    ImGui::PushID(id);
+}
+
+void elisa_imgui_pop_id() {
+    ImGui::PopID();
+}
+
+void elisa_imgui_push_style_color_u32(int idx, uint32_t color) {
+    ImGui::PushStyleColor(static_cast<ImGuiCol>(idx), color);
+}
+
+void elisa_imgui_pop_style_color(int count) {
+    ImGui::PopStyleColor(count);
+}
+
+void elisa_imgui_push_style_var_vec2(int idx, float x, float y) {
+    ImGui::PushStyleVar(static_cast<ImGuiStyleVar>(idx), ImVec2{x, y});
+}
+
+void elisa_imgui_pop_style_var(int count) {
+    ImGui::PopStyleVar(count);
+}
+
+float elisa_imgui_get_text_line_height() {
+    return ImGui::GetTextLineHeight();
+}
+
+float elisa_imgui_get_text_line_height_with_spacing() {
+    return ImGui::GetTextLineHeightWithSpacing();
+}
+
+float elisa_imgui_get_frame_height_with_spacing() {
+    return ImGui::GetFrameHeightWithSpacing();
+}
+
+ElisaImGuiVec2 elisa_imgui_get_content_region_avail() {
+    const ImVec2 value = ImGui::GetContentRegionAvail();
+    return ElisaImGuiVec2{value.x, value.y};
+}
+
+ElisaImGuiVec2 elisa_imgui_get_cursor_screen_pos() {
+    const ImVec2 value = ImGui::GetCursorScreenPos();
+    return ElisaImGuiVec2{value.x, value.y};
+}
+
+void elisa_imgui_set_cursor_pos_x(float x) {
+    ImGui::SetCursorPosX(x);
+}
+
+void elisa_imgui_dummy(float x, float y) {
+    ImGui::Dummy(ImVec2{x, y});
+}
+
+bool elisa_imgui_is_item_hovered() {
+    return ImGui::IsItemHovered();
+}
+
+bool elisa_imgui_is_item_active() {
+    return ImGui::IsItemActive();
+}
+
+bool elisa_imgui_is_mouse_clicked(int button) {
+    return ImGui::IsMouseClicked(button);
+}
+
+bool elisa_imgui_is_mouse_released(int button) {
+    return ImGui::IsMouseReleased(button);
+}
+
+bool elisa_imgui_begin_popup(const char* id) {
+    return ImGui::BeginPopup(id);
+}
+
+void elisa_imgui_end_popup() {
+    ImGui::EndPopup();
+}
+
+void elisa_imgui_open_popup(const char* id) {
+    ImGui::OpenPopup(id);
+}
+
+void elisa_imgui_set_next_item_width(float width) {
+    ImGui::SetNextItemWidth(width);
+}
+
+bool elisa_imgui_begin_tooltip() {
+    return ImGui::BeginTooltip();
+}
+
+void elisa_imgui_end_tooltip() {
+    ImGui::EndTooltip();
+}
+
+const char* elisa_imgui_get_clipboard_text() {
+    return ImGui::GetClipboardText();
+}
+
+void elisa_imgui_set_clipboard_text(const char* text) {
+    ImGui::SetClipboardText(text);
+}
+
+} // extern "C"
+
---
+++ core/devtools/widget/imgui_c_api.h
@@ -0,0 +1,80 @@
+// SPDX-FileCopyrightText: Copyright 2026 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct ElisaImGuiVec2 {
+    float x;
+    float y;
+} ElisaImGuiVec2;
+
+typedef struct ElisaImGuiVec4 {
+    float x;
+    float y;
+    float z;
+    float w;
+} ElisaImGuiVec4;
+
+void elisa_imgui_set_next_window_size(float x, float y, int cond);
+bool elisa_imgui_begin(const char* name, bool* open, int flags);
+void elisa_imgui_end(void);
+bool elisa_imgui_begin_child(const char* id, float x, float y, bool border, int flags);
+void elisa_imgui_end_child(void);
+bool elisa_imgui_begin_table(const char* id, int columns, int flags);
+void elisa_imgui_end_table(void);
+void elisa_imgui_table_setup_column(const char* label, int flags, float init_width_or_weight);
+void elisa_imgui_table_headers_row(void);
+void elisa_imgui_table_next_row(void);
+void elisa_imgui_table_next_column(void);
+void elisa_imgui_table_set_column_index(int column);
+void elisa_imgui_text_unformatted(const char* text);
+void elisa_imgui_text(const char* fmt);
+void elisa_imgui_text_colored(float r, float g, float b, float a, const char* text);
+bool elisa_imgui_button(const char* label);
+bool elisa_imgui_checkbox(const char* label, bool* value);
+bool elisa_imgui_input_text(const char* label, char* buffer, uintptr_t buffer_size, int flags);
+bool elisa_imgui_tree_node(const void* id, const char* label);
+void elisa_imgui_tree_pop(void);
+bool elisa_imgui_selectable(const char* label, bool selected, int flags, float x, float y);
+bool elisa_imgui_collapsing_header(const char* label, int flags);
+void elisa_imgui_same_line(float offset_from_start_x, float spacing);
+void elisa_imgui_separator(void);
+void elisa_imgui_separator_text(const char* label);
+void elisa_imgui_push_id_int(int id);
+void elisa_imgui_push_id_ptr(const void* id);
+void elisa_imgui_pop_id(void);
+void elisa_imgui_push_style_color_u32(int idx, uint32_t color);
+void elisa_imgui_pop_style_color(int count);
+void elisa_imgui_push_style_var_vec2(int idx, float x, float y);
+void elisa_imgui_pop_style_var(int count);
+float elisa_imgui_get_text_line_height(void);
+float elisa_imgui_get_text_line_height_with_spacing(void);
+float elisa_imgui_get_frame_height_with_spacing(void);
+ElisaImGuiVec2 elisa_imgui_get_content_region_avail(void);
+ElisaImGuiVec2 elisa_imgui_get_cursor_screen_pos(void);
+void elisa_imgui_set_cursor_pos_x(float x);
+void elisa_imgui_dummy(float x, float y);
+bool elisa_imgui_is_item_hovered(void);
+bool elisa_imgui_is_item_active(void);
+bool elisa_imgui_is_mouse_clicked(int button);
+bool elisa_imgui_is_mouse_released(int button);
+bool elisa_imgui_begin_popup(const char* id);
+void elisa_imgui_end_popup(void);
+void elisa_imgui_open_popup(const char* id);
+void elisa_imgui_set_next_item_width(float width);
+bool elisa_imgui_begin_tooltip(void);
+void elisa_imgui_end_tooltip(void);
+const char* elisa_imgui_get_clipboard_text(void);
+void elisa_imgui_set_clipboard_text(const char* text);
+
+#ifdef __cplusplus
+}
+#endif
+
